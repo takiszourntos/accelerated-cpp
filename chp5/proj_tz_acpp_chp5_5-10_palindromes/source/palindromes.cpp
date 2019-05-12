f6f194aed91ce743c8defb92db9dfe545b743d64
Takis Zourntos <takis.zourntos@emads.org>
2019-05-12 17:26:48 -0400

    completed Chapter 5

--- chp5/proj_tz_acpp_chp5_5-10_palindromes/source/palindromes.cpp
+++ chp5/proj_tz_acpp_chp5_5-10_palindromes/source/palindromes.cpp
@@ -1,17 +1,23 @@
 /*
- * palindromes.cpp
- *
- *  Created on: May 12, 2019
- *      Author: takis
+**************************************************************************************
+* palindrome.cpp:                                                                    *
+*                                                                                    *
+* this program finds all the palindromes in a dictionary, found in a file ./dict.txt *
+*                     (dictionary found at: https://github.com/dwyl)                 *
+* author: Takis Zourntos                                                             *
+* date: May 2019                                                                     *
+**************************************************************************************
  */
 
 #include <string>
 #include <list>
 #include <iostream>
+#include <algorithm>
 #include "dictionary.h"
 
 using std::string;				using std::list;
 using std::cout;				using std::endl;
+using std::max;
 
 int main()
 {
@@ -25,19 +31,28 @@ int main()
 		return 1;
 	}
 
-	// display the palindromes
+	// display the palindromes, while you're at it, find the longest palindrome
 	list<string> palindromes;
 	find_palin(dict, palindromes); // store the palindromes here
 	cout << endl << "Here are the palindromes in the file " << " :" << endl;
 	list<string>::const_iterator iter_pd = palindromes.begin();
-
+	list<string>::size_type maxlen = 0;
+	list<string>::size_type currentlen = 0;
+	string max_palindrome="";
 	// invariant: iter_pd points to the palindrome to be printed
 	while (iter_pd != palindromes.end())
 	{
+		currentlen = (*iter_pd).size();
+		if (currentlen > maxlen)
+		{
+			maxlen = currentlen;
+			max_palindrome = *iter_pd;
+		}
 		cout << *iter_pd << endl;
 		++iter_pd;
 	}
 	cout << endl;
+	cout << "And, by the way, the longest palindrome is " << max_palindrome << " at length of " << maxlen << " characters." << endl;
 	return 0;
 }
 
