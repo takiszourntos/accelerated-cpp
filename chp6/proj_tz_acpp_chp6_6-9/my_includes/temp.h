1aa2f209a6b9f5849420b42f0258d0036602ba5b
Takis Zourntos <tzourntos@mac.com>
2019-06-21 16:05:34 -0400

    experimenting with sharing projects

---
+++ chp6/proj_tz_acpp_chp6_6-9/my_includes/temp.h
@@ -0,0 +1,15 @@
+/*
+ * temp.h
+ *
+ *  Created on: Jun. 21, 2019
+ *      Author: takis
+ */
+
+#ifndef MY_INCLUDES_TEMP_H_
+#define MY_INCLUDES_TEMP_H_
+
+
+
+
+
+#endif /* MY_INCLUDES_TEMP_H_ */
---
+++ chp6/test_project/src/test_project.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : test_project.cpp
+// Author      : takis
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "temp.h"
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
